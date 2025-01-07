ea61ca81c26ec4fdcafb0c92da9d6b5204327c9b
Nahian Chowdhury <nahianchowdhury00777@gmail.com>
2025-01-07 12:32:51 +0600

    backend folder structure

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const app = express();
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const URI = process.env.MONGO_URI;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// connect to mongoDB
+try {
+  mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("Connected to the database");
+} catch (error) {
+  console.error("Error connecting to the database: ", error.message);
+}
 
 app.listen(PORT, () => {
   console.log(`Example of listening at http://localhost:${PORT}`);
