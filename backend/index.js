b8868616db52fa28e3cecbf3d726eb804878052e
Nahian Chowdhury <nahianchowdhury00777@gmail.com>
2025-01-07 12:19:51 +0600

    project structure change

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example of listening at http://localhost:${PORT}`);
+});
