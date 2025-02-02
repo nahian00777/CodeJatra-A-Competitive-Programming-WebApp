d15e84677111b973b871583faae2bf378aecb97d
Sojib001 <sajibbhattacharjee128@gmail.com>
2025-02-02 21:20:16 +0600

    added name in problem model

--- backend/models/problem.model.js
+++ backend/models/problem.model.js
@@ -4,6 +4,7 @@ const problemSchema = new mongoose.Schema({
   contestId: String,
   index: String,
   rating: Number,
+  name: String,
 });
 
 export const Problem = mongoose.model("Problem", problemSchema);
